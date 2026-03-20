dfe9c53cfcb398c4fe587cc461b63a241b407e89
steven <12992@holbertonstudents.com>
2026-03-20 16:15:40 +0100

    mise en place de l'espace de travail : printf_c et main.h

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdarg.h>
+
+
+
+/*
+*
+*
+* 
+*
+*
+*/
+
+
+
+
+
+
+
+int _printf(const char *format, ...)
+{
+
+/*
+*
+* Tableau de fonction ici
+*
+*/
+
+
+}
---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+/*
+*
+* Add ici la structure qui servira plus tard en comparateur
+*
+*/
+
+
+/*
+*
+* Add ici les prototypes de fonctions qui seront utilisées
+*/
+
+int _printf(const char *format, ...);
+
+#endif
